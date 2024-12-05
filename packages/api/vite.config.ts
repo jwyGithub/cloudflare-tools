f915617a9b0c9d091a9ca374df499a1c180d323a
jiangwy <jiangwy@staryea.com>
2024-12-05 10:10:23 +0800

    api-fix: fix export

--- packages/api/vite.config.ts
+++ packages/api/vite.config.ts
@@ -5,7 +5,7 @@ import dts from 'vite-plugin-dts';
 export default defineConfig({
     build: {
         lib: {
-            entry: [resolve(__dirname, './src/index.ts')],
+            entry: [resolve(__dirname, './index.ts')],
             name: 'WorkerApi'
         },
         target: ['es2022'],
